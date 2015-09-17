631859ba6b39fce0c5ddb12e28ed95508881c1f5
Daniel Luna <dluna9081@gmail.com>
2015-09-17 01:46:43 -0700

    changed module name for dokku

---
+++ metallica-tour-map.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import getdata
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('main.html', coordinates = getdata.coordinates, 
+    						concerts = getdata.concerts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
